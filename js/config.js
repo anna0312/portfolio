56434f0d2d360e5060d325ab29cbc5863c180550
anna0312 <annabremner@gmail.com>
2018-04-26 01:42:39 -0400

    Completed first draft of site

---
+++ js/config.js
@@ -0,0 +1,22 @@
+/******************************************************************************************************************/
+/* BRAX | Responsive Personal Portfolio Template                                                                  */
+/* (c) 2018 Rene Puchinger                                                                                        */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["Full stack developer.", "Front end designer", "Builder of cool stuff", "Junior frommagier."];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Enable typed effect  */
+var config_typed = false;
+
+/* Google map parameters */
+
+var config_mapLat = 42.3601; // latitude
+var config_mapLng = -71.0589; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'Me' // marker title
